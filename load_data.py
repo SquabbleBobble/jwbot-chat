7d81074afd15b6e470d95f0f8eaa8ddb72351432
SquabbleBobble <95829996+SquabbleBobble@users.noreply.github.com>
2025-06-17 17:26:28 -0500

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,1 +1,5 @@
-# Loads pre-chunked jw.org data
+import pickle
+
+def load_vectorstore():
+    with open("vector_store.pkl", "rb") as f:
+        return pickle.load(f)# Loads pre-chunked jw.org data
