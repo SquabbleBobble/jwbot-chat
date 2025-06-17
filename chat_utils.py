a45b8576fdd9c3cd23da0117de82a63f8599f8f7
SquabbleBobble <95829996+SquabbleBobble@users.noreply.github.com>
2025-06-17 17:23:32 -0500

    Update chat_utils.py

--- chat_utils.py
+++ chat_utils.py
@@ -1,1 +1,4 @@
-# Search and response logic
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI# Search and response logic
